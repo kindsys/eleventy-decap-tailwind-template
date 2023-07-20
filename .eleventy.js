b418e79bcc14b49e3dd5f02b6d77b6654e7f5dbf
Bella Jariel <bjariel@hmc.edu>
2023-07-20 15:59:23 -0400

    Add templates, configure Decap

---
+++ .eleventy.js
@@ -0,0 +1,11 @@
+const eleventyNavigationPlugin = require("@11ty/eleventy-navigation");
+
+module.exports = function (eleventyConfig) {
+    eleventyConfig.addPlugin(eleventyNavigationPlugin);
+    return {
+      dir: {
+        input: 'src',
+        output: '_site',
+      },
+    };
+  };
