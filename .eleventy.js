5f44dab6cc29df77585f997d83134b0ff3abd256
Bella Jariel <bjariel@hmc.edu>
2023-07-25 11:45:35 -0400

    Webpack

--- .eleventy.js
+++ .eleventy.js
@@ -7,7 +7,6 @@ module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy('src/img')
   eleventyConfig.addPassthroughCopy("src/admin")
 
-
   const {
     DateTime
   } = require("luxon");
